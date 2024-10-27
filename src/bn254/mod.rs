67c20f84a2f33596df3dbf6df9eb1de65b809396
bbist <pro.bbist@gmail.com>
2024-10-27 23:59:38 +0545

    superblock invalidation script chunk logic

--- src/bn254/mod.rs
+++ src/bn254/mod.rs
@@ -10,12 +10,13 @@ pub mod pairing;
 
 pub mod ell_coeffs;
 
+pub mod chunk_compile;
+pub mod chunk_config;
+pub mod chunk_evaluate;
+pub mod chunk_msm;
+pub mod chunk_primitves;
+pub mod chunk_superblock;
+pub mod chunk_taps;
 pub mod fp254impl;
 pub mod msm;
 pub mod utils;
-pub mod chunk_primitves;
-pub mod chunk_taps;
-pub mod chunk_config;
-pub mod chunk_compile;
-pub mod chunk_evaluate;
-pub mod chunk_msm;
