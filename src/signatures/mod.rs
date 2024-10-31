a4b35505d0e0cacc081986e9c4df01144f1349ce
Manish Bista <manishbista@Manishs-MacBook-Pro.local>
2024-10-31 23:46:48 +0545

    wip: sync wots

--- src/signatures/mod.rs
+++ src/signatures/mod.rs
@@ -2,4 +2,3 @@ pub mod winternitz;
 pub mod winternitz_compact;
 pub mod winternitz_hash;
 pub mod wots;
-pub mod winternitz_compact_hash;
--- src/signatures/winternitz.rs
+++ src/signatures/winternitz.rs
@@ -19,39 +19,25 @@ use crate::treepp::*;
 use bitcoin::hashes::{hash160, Hash};
 use hex::decode as hex_decode;
 
-///
-const N_BITS: u32 = 256;
 /// Bits per digit
 const LOG_D: u32 = 4;
 /// Digits are base d+1
 pub const D: u32 = (1 << LOG_D) - 1;
 /// Number of digits of the message
-const N0: u32 = (N_BITS + LOG_D - 1) / LOG_D;
-/// Number of digits of the checksum
-const N1: usize = log(D * N0, D + 1) as usize;
+const N0: u32 = 40;
+/// Number of digits of the checksum.  N1 = ⌈log_{D+1}(D*N0)⌉ + 1
+const N1: usize = 4;
 /// Total number of digits to be signed
 const N: u32 = N0 + N1 as u32;
-
-const N2: usize = 20;
 /// The public key type
-pub type PublicKey = [[u8; N2]; N as usize];
+pub type PublicKey = [[u8; 20]; N as usize];
+
 //
 // Helper functions
 //
-const fn log(n: u32, base: u32) -> u32 {
-    _ = n - 1; // compile time assertion: self >= 1
-    _ = base - 2; // compile time assertion: base >= 2
-    let mut res = 0;
-    let mut power = 1;
-    while power < n {
-        res += 1;
-        power *= base;
-    }
-    res
-}
 
 /// Generate a public key for the i-th digit of the message
-pub fn public_key_for_digit(secret_key: &str, digit_index: u32) -> [u8; N2] {
+pub fn public_key_for_digit(secret_key: &str, digit_index: u32) -> [u8; 20] {
     // Convert secret_key from hex string to bytes
     let mut secret_i = match hex_decode(secret_key) {
         Ok(bytes) => bytes,
@@ -69,9 +55,9 @@ pub fn public_key_for_digit(secret_key: &str, digit_index: u32) -> [u8; N2] {
     *hash.as_byte_array()
 }
 
-/// Generate a public key from a secret key 
+/// Generate a public key from a secret key
 pub fn generate_public_key(secret_key: &str) -> PublicKey {
-    let mut public_key_array = [[0u8; N2]; N as usize];
+    let mut public_key_array = [[0u8; 20]; N as usize];
     for i in 0..N {
         public_key_array[i as usize] = public_key_for_digit(secret_key, i);
     }
@@ -79,7 +65,16 @@ pub fn generate_public_key(secret_key: &str) -> PublicKey {
 }
 
 /// Compute the signature for the i-th digit of the message
-pub fn digit_signature(secret_key: &str, digit_index: u32, message_digit: u8) -> Vec<Script> {
+pub fn digit_signature(secret_key: &str, digit_index: u32, message_digit: u8) -> Script {
+    let hash_bytes = digit_signature_witness(secret_key, digit_index, message_digit);
+
+    script! {
+        { hash_bytes }
+        { message_digit }
+    }
+}
+
+pub fn digit_signature_witness(secret_key: &str, digit_index: u32, message_digit: u8) -> Vec<u8> {
     // Convert secret_key from hex string to bytes
     let mut secret_i = match hex_decode(secret_key) {
         Ok(bytes) => bytes,
@@ -96,13 +91,7 @@ pub fn digit_signature(secret_key: &str, digit_index: u32, message_digit: u8) ->
 
     let hash_bytes = hash.as_byte_array().to_vec();
 
-    let sc1 = script! {
-        { hash_bytes }
-    };
-    let sc2 = script! {
-        { message_digit }
-    };
-    vec![sc1, sc2]
+    hash_bytes
 }
 
 /// Compute the checksum of the message's digits.
@@ -126,48 +115,116 @@ pub fn to_digits<const DIGIT_COUNT: usize>(mut number: u32) -> [u8; DIGIT_COUNT]
     digits
 }
 
-
-
 /// Compute the signature for a given message
-pub fn sign_digits(secret_key: &str, message_digits: [u8; N0 as usize]) -> Vec<Script> {
+pub fn sign_digits(secret_key: &str, message_digits: [u8; N0 as usize]) -> Script {
     // const message_digits = to_digits(message, n0)
     let mut checksum_digits = to_digits::<N1>(checksum(message_digits)).to_vec();
     checksum_digits.append(&mut message_digits.to_vec());
 
-    let mut scs: Vec<Script> = Vec::new();
-    for i in 0..N {
-        for s in digit_signature(secret_key, i, checksum_digits[ (N-1-i) as usize]) {
-            scs.push(s);
+    script! {
+        for i in 0..N {
+            { digit_signature(secret_key, i, checksum_digits[ (N-1-i) as usize]) }
         }
     }
-    scs
+}
+
+/// Compute the signature for a given message
+pub fn sign_digits_witness(secret_key: &str, message_digits: [u8; N0 as usize]) -> Vec<Vec<u8>> {
+    // const message_digits = to_digits(message, n0)
+    let mut checksum_digits = to_digits::<N1>(checksum(message_digits)).to_vec();
+    checksum_digits.append(&mut message_digits.to_vec());
+
+    let res: Vec<Vec<u8>> = (0..N)
+        .map(|i| digit_signature_witness(secret_key, i, checksum_digits[(N - 1 - i) as usize]))
+        .collect();
+    res
 }
 
 pub fn sign(secret_key: &str, message_bytes: &[u8]) -> Script {
     // Convert message to digits
-    let mut message_digits = [0u8; N0 as usize];
+    let mut message_digits = [0u8; 20 * 2 as usize];
     for (digits, byte) in message_digits.chunks_mut(2).zip(message_bytes) {
         digits[0] = byte & 0b00001111;
         digits[1] = byte >> 4;
     }
 
-    let scs = sign_digits(secret_key, message_digits);
-    let mut sc = script!{};
-    for s in scs {
-        sc = sc.push_script(s.compile());
-    }
-    sc
+    sign_digits(secret_key, message_digits)
 }
 
-pub fn checksig_verify(public_key: &PublicKey) -> Script {
-    script! {
-        //
-        // Verify the hash chain for each digit
-        //
+pub fn sign_witness(secret_key: &str, message_bytes: &[u8]) -> Vec<Vec<u8>> {
+    // Convert message to digits
+    let mut message_digits = [0u8; 20 * 2 as usize];
+    for (digits, byte) in message_digits.chunks_mut(2).zip(message_bytes) {
+        digits[0] = byte & 0b00001111;
+        digits[1] = byte >> 4;
+    }
 
-        // Repeat this for every of the n many digits
-        for digit_index in 0..N {
+    sign_digits_witness(secret_key, message_digits)
+}
+
+pub fn verify_digit(digit_pubkey: &[u8; 20]) -> Script {
+    if LOG_D == 4 {
+        // when using 4-bit digits, we apply an optimization. In the unoptimized version:
+        // - we push 16 hashes on the stack (OP_DUP + OP_HASH160)
+        // - use op_pick
+        // - do 8 OP_2DROPS to clear the stack
+        // In the optimized version, we save 8 OP_DUP and 4 OP_2DROPS, in exchange for 9 opcodes
+        // to implement to following logic (saving 3 bytes per digit):
+        // if the digit is less than 8:
+        //     we know that the hash that we're interested in is not in the first 8 hashes. So we
+        //     do the the first 8 hashes without storing intermediate results.
+        // else:
+        //     we know that the hash that we're interested in is not in the last 8 hashes. So we
+        //     just just reduce the op_pick input by 8
+        // Now we only need to do 8 iterations of [OP_DUP OP_HASH160], saving 8 OP_DUPS and 4 OP_2DROPS
+        script! {
             // Verify that the digit is in the range [0, d]
+            // See https://github.com/BitVM/BitVM/issues/35
+            { D }
+            OP_MIN
+
+            // Push a copy of the digit onto the altstack
+            OP_DUP
+            OP_TOALTSTACK
+            // push a copy of the digit on the stack
+            OP_DUP
+
+            OP_8
+            OP_LESSTHAN
+            OP_IF
+                // digit is less than 8: run 8 iterations of the hashing without storing intermediate results on stack
+                // The stack index to use will be equal to the digit.
+                OP_TOALTSTACK
+                for _ in 0..8 {
+                    OP_HASH160
+                }
+            OP_ELSE
+                // digit is 8 or more - we don't need to run the last 8 iterations of the hashing.
+                // Reduce the stack index by 8 to compensate
+                OP_8
+                OP_SUB
+                OP_TOALTSTACK
+            OP_ENDIF
+
+            // Hash the input hash d-8 times and put every result on the stack
+            for _ in 0..D-8 {
+                OP_DUP OP_HASH160
+            }
+
+            // Verify the signature for this digit
+            OP_FROMALTSTACK
+            OP_PICK
+            { digit_pubkey.to_vec() }
+            OP_EQUALVERIFY
+
+            // Drop the d+1 stack items
+            for _ in 0..(D+1)/2/2 {
+                OP_2DROP
+            }
+        }
+    } else {
+        script! {
+             // Verify that the digit is in the range [0, d]
             // See https://github.com/BitVM/BitVM/issues/35
             { D }
             OP_MIN
@@ -185,13 +242,26 @@ pub fn checksig_verify(public_key: &PublicKey) -> Script {
             // Verify the signature for this digit
             OP_FROMALTSTACK
             OP_PICK
-            { public_key[N as usize - 1 - digit_index as usize].to_vec() }
+            { digit_pubkey.to_vec() }
             OP_EQUALVERIFY
 
             // Drop the d+1 stack items
             for _ in 0..(D+1)/2 {
                 OP_2DROP
             }
+        }
+    }
+}
+
+pub fn checksig_verify(public_key: &PublicKey) -> Script {
+    script! {
+        //
+        // Verify the hash chain for each digit
+        //
+
+        // Repeat this for every of the n many digits
+        for digit_index in 0..N {
+            { verify_digit(&public_key[N as usize - 1 - digit_index as usize]) }
         }
 
         //
@@ -221,36 +291,32 @@ pub fn checksig_verify(public_key: &PublicKey) -> Script {
         OP_EQUALVERIFY
 
 
-        // // Convert the message's digits to bytes
-        // for i in 0..N0 / 2 {
-        //     OP_SWAP
-        //     for _ in 0..LOG_D {
-        //         OP_DUP OP_ADD
-        //     }
-        //     OP_ADD
-        //     // Push all bytes to the altstack, except for the last byte
-        //     if i != (N0/2) - 1 {
-        //         OP_TOALTSTACK
-        //     }
-        // }
-        // // Read the bytes from the altstack
-        // for _ in 0..N0 / 2 - 1{
-        //     OP_FROMALTSTACK
-        // }
+        // Convert the message's digits to bytes
+        for i in 0..N0 / 2 {
+            OP_SWAP
+            for _ in 0..LOG_D {
+                OP_DUP OP_ADD
+            }
+            OP_ADD
+            // Push all bytes to the altstack, except for the last byte
+            if i != (N0/2) - 1 {
+                OP_TOALTSTACK
+            }
+        }
+        // Read the bytes from the altstack
+        for _ in 0..N0 / 2 - 1{
+            OP_FROMALTSTACK
+        }
 
     }
 }
 
-
 #[cfg(test)]
 mod test {
-    use bitcoin::ScriptBuf;
-
     use super::*;
 
     // The secret key
     const MY_SECKEY: &str = "b138982ce17ac813d505b5b40b665d404e9528e7";
-
 
     #[test]
     fn test_winternitz() {
@@ -259,8 +325,6 @@ mod test {
         const MESSAGE: [u8; N0 as usize] = [
             1, 2, 3, 4, 5, 6, 7, 8, 9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 7, 7, 7, 7, 7,
             1, 2, 3, 4, 5, 6, 7, 8, 9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 7, 7, 7, 7, 7,
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 7, 7, 7, 7, 7,
-            1, 2, 3, 4,
         ];
 
         let public_key = generate_public_key(MY_SECKEY);
@@ -270,14 +334,6 @@ mod test {
             { checksig_verify(&public_key) }
         };
 
-        let sig2 = sign_digits(MY_SECKEY, MESSAGE) ;
-        let mut filtered_sig2 = script!{};
-        for i in 0..sig2.len() {
-            if i%2 == 0 {
-                filtered_sig2 = filtered_sig2.push_script(sig2[i].clone().compile());
-            }
-        }
-
         println!(
             "Winternitz signature size:\n \t{:?} bytes / {:?} bits \n\t{:?} bytes / bit",
             script.len(),
@@ -285,39 +341,34 @@ mod test {
             script.len() as f64 / (N0 * 4) as f64
         );
 
-        let sc = script! {
-            {filtered_sig2 }
-            // { checksig_verify(&public_key) }
+        let result = execute_script(script! {
+            { sign_digits(MY_SECKEY, MESSAGE) }
+            { checksig_verify(&public_key) }
 
-            // 0x21 OP_EQUALVERIFY
-            // 0x43 OP_EQUALVERIFY
-            // 0x65 OP_EQUALVERIFY
-            // 0x87 OP_EQUALVERIFY
-            // 0xA9 OP_EQUALVERIFY
-            // 0xCB OP_EQUALVERIFY
-            // 0xED OP_EQUALVERIFY
-            // 0x7F OP_EQUALVERIFY
-            // 0x77 OP_EQUALVERIFY
-            // 0x77 OP_EQUALVERIFY
+            0x21 OP_EQUALVERIFY
+            0x43 OP_EQUALVERIFY
+            0x65 OP_EQUALVERIFY
+            0x87 OP_EQUALVERIFY
+            0xA9 OP_EQUALVERIFY
+            0xCB OP_EQUALVERIFY
+            0xED OP_EQUALVERIFY
+            0x7F OP_EQUALVERIFY
+            0x77 OP_EQUALVERIFY
+            0x77 OP_EQUALVERIFY
 
-            // 0x21 OP_EQUALVERIFY
-            // 0x43 OP_EQUALVERIFY
-            // 0x65 OP_EQUALVERIFY
-            // 0x87 OP_EQUALVERIFY
-            // 0xA9 OP_EQUALVERIFY
-            // 0xCB OP_EQUALVERIFY
-            // 0xED OP_EQUALVERIFY
-            // 0x7F OP_EQUALVERIFY
-            // 0x77 OP_EQUALVERIFY
-            // 0x77 OP_EQUAL
-        };
-        
-        let comp = sc.clone().compile();
-        println!("script {:?}", comp);
-        let res = execute_script(sc);
-        for i in 0..res.final_stack.len() {
-            println!("{i:3} {:?}", res.final_stack.get(i));
-        }
+            0x21 OP_EQUALVERIFY
+            0x43 OP_EQUALVERIFY
+            0x65 OP_EQUALVERIFY
+            0x87 OP_EQUALVERIFY
+            0xA9 OP_EQUALVERIFY
+            0xCB OP_EQUALVERIFY
+            0xED OP_EQUALVERIFY
+            0x7F OP_EQUALVERIFY
+            0x77 OP_EQUALVERIFY
+            0x77 OP_EQUAL
+        });
+
+        assert!(result.success);
     }
 
     // TODO: test the error cases: negative digits, digits > D, ...
