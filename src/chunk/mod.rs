bedc491a02f28434cd280082775dd456b71a8f0b
Manish Bista <manishbista@Manishs-MBP.lan>
2025-01-25 14:03:14 +0545

    cargo fix imports 2

--- src/chunk/mod.rs
+++ src/chunk/mod.rs
@@ -2,20 +2,15 @@ pub mod api;
 mod blake3compiled;
 pub mod compile;
 pub mod element;
-#[allow(unused_variables)]
 mod taps_msm;
 mod primitves;
-#[allow(unused_variables)]
 mod taps_point_ops;
-#[allow(unused_variables)]
 mod taps_mul;
 mod taps_test;
 mod wots;
 pub mod assert;
 pub mod segment;
-#[allow(unused_variables)]
 mod taps_premiller;
-#[allow(unused_variables)]
 mod taps_point_eval;
 mod taps_subgroup_check;
 mod assigner;
