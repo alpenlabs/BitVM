5b9d154f666371c59005d9fe6550faa706ccf175
Manish Bista <manishbista@Manishs-MBP.lan>
2025-01-25 19:11:13 +0545

    init: miller algorithm

--- src/bn254/pairing.rs
+++ src/bn254/pairing.rs
@@ -1073,6 +1073,7 @@ mod test {
 
             // affine mode
             let a_prepared = G2Prepared::from_affine(a);
+            println!("a_prepared {:?}", a_prepared);
             let a_affine = a;
 
             let miller_loop = Pairing::miller_loop(&a_prepared, true);
@@ -1091,6 +1092,160 @@ mod test {
         }
     }
 
+
+    use ark_ec::pairing::{MillerLoopOutput};
+
+
+    use core::ops::Neg;
+
+    use ark_ec::{bn::BnConfig,  CurveGroup};
+
+    // p1 should have been precomputed
+    fn bn254_mm(p1: ark_bn254::G1Affine, q1:ark_bn254::G2Affine) -> ark_bn254::fq12::Fq12 {
+        let beta_12x = BigUint::from_str(
+            "21575463638280843010398324269430826099269044274347216827212613867836435027261",
+        )
+        .unwrap();
+        let beta_12y = BigUint::from_str(
+            "10307601595873709700152284273816112264069230130616436755625194854815875713954",
+        )
+        .unwrap();
+        let beta_12 = ark_bn254::Fq2::from_base_prime_field_elems([
+            ark_bn254::Fq::from(beta_12x.clone()),
+            ark_bn254::Fq::from(beta_12y.clone()),
+        ])
+        .unwrap();
+        let beta_13x = BigUint::from_str(
+            "2821565182194536844548159561693502659359617185244120367078079554186484126554",
+        )
+        .unwrap();
+        let beta_13y = BigUint::from_str(
+            "3505843767911556378687030309984248845540243509899259641013678093033130930403",
+        )
+        .unwrap();
+        let beta_13 = ark_bn254::Fq2::from_base_prime_field_elems([
+            ark_bn254::Fq::from(beta_13x.clone()),
+            ark_bn254::Fq::from(beta_13y.clone()),
+        ])
+        .unwrap();
+        let beta_22x = BigUint::from_str(
+            "21888242871839275220042445260109153167277707414472061641714758635765020556616",
+        )
+        .unwrap();
+        let beta_22y = BigUint::from_str("0").unwrap();
+        let beta_22 = ark_bn254::Fq2::from_base_prime_field_elems([
+            ark_bn254::Fq::from(beta_22x.clone()),
+            ark_bn254::Fq::from(beta_22y.clone()),
+        ])
+        .unwrap();
+
+        let mut f = ark_bn254::Fq12::ONE;
+        let mut t = q1;
+        let p = ark_bn254::G1Affine::new_unchecked(-p1.x/p1.y, p1.y.inverse().unwrap());
+        for i in (1..ark_bn254::Config::ATE_LOOP_COUNT.len()).rev() {
+            let ate_bit = ark_bn254::Config::ATE_LOOP_COUNT[i - 1];
+            // square
+            f = f * f;
+
+                // println!("G sq ate {:?}  value {:?}", ate_bit, f);
+
+
+            // double and eval
+            let alpha = (t.x.square() + t.x.square() + t.x.square()) / (t.y + t.y); 
+            let neg_bias = alpha * t.x - t.y;
+            let mut le0 = alpha;
+            le0.mul_assign_by_fp(&p.x);
+            let mut le1 = neg_bias;
+            le1.mul_assign_by_fp(&p.y);
+            let mut le = ark_bn254::Fq12::ZERO;
+            le.c0.c0 = ark_bn254::fq2::Fq2::ONE;
+            le.c1.c0 = le0;
+            le.c1.c1 = le1;
+
+            f = f * le;
+
+            t = (t + t).into_affine();
+
+
+            if ate_bit == 1 || ate_bit == -1 {
+                let mut q = q1.clone();
+                if ate_bit == -1 {
+                    q = q.neg();
+                };
+                let alpha = (t.y - q.y) / (t.x - q.x);
+                let neg_bias = alpha * t.x - t.y;
+
+                let mut le0 = alpha;
+                le0.mul_assign_by_fp(&p.x);
+                let mut le1 = neg_bias;
+                le1.mul_assign_by_fp(&p.y);
+                let mut le = ark_bn254::Fq12::ZERO;
+                le.c0.c0 = ark_bn254::fq2::Fq2::ONE;
+                le.c1.c0 = le0;
+                le.c1.c1 = le1;
+
+                f = f * le;
+
+                t = (t + q).into_affine();
+        
+            }
+        }
+        // t + q^2
+        let mut q = q1.clone();
+        q.x.conjugate_in_place();
+        q.x = q.x * beta_12;
+        q.y.conjugate_in_place();
+        q.y = q.y * beta_13;
+        let alpha = (t.y - q.y) / (t.x - q.x);
+        let neg_bias = alpha * t.x - t.y;
+        let mut le0 = alpha;
+        le0.mul_assign_by_fp(&p.x);
+        let mut le1 = neg_bias;
+        le1.mul_assign_by_fp(&p.y);
+        let mut le = ark_bn254::Fq12::ZERO;
+        le.c0.c0 = ark_bn254::fq2::Fq2::ONE;
+        le.c1.c0 = le0;
+        le.c1.c1 = le1;
+
+        f = f * le;
+
+        t = (t + q).into_affine();
+
+        // t + q^3
+
+        let mut q = q1.clone();
+        q.x = q.x * beta_22;
+
+        let alpha = (t.y - q.y) / (t.x - q.x);
+        let neg_bias = alpha * t.x - t.y;
+        let mut le0 = alpha;
+        le0.mul_assign_by_fp(&p.x);
+        let mut le1 = neg_bias;
+        le1.mul_assign_by_fp(&p.y);
+        let mut le = ark_bn254::Fq12::ZERO;
+        le.c0.c0 = ark_bn254::fq2::Fq2::ONE;
+        le.c1.c0 = le0;
+        le.c1.c1 = le1;
+
+        f = f * le;
+
+        //t = (t + q).into_affine();
+
+        f
+    }
+
+    #[test]
+    fn test_mm() {
+        let rng = &mut test_rng();
+        let p = ark_bn254::G1Affine::rand(rng);
+        let q = ark_bn254::G2Affine::rand(rng);
+        let d = bn254_mm(p, q);
+        let e = Bn254::multi_miller_loop_affine([p], [q]).0;
+        println!("a_prepared {:?}", d);
+        println!("a_prepared {:?}", e);
+    }
+
+    
     #[test]
     fn test_dual_miller_loop_with_c_wi() {
         let mut prng = ChaCha20Rng::seed_from_u64(0);
