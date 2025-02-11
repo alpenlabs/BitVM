0853a508a32bb0a3989a82780c51b19e3850ddac
Manish Bista <manishbista@Manishs-MacBook-Pro.local>
2025-02-11 17:16:43 +0545

    remove comments

--- bitvm/src/chunk/mod.rs
+++ bitvm/src/chunk/mod.rs
@@ -2,17 +2,14 @@ pub mod api;
 mod blake3compiled;
 pub mod compile;
 pub mod elements;
-// #[allow(unused_variables)]
 mod taps_msm;
 mod primitives;
-// #[allow(unused_variables)]
 mod taps_point_ops;
 mod taps_test;
 pub mod norm_fp12;
 mod wots;
 pub mod assert;
 pub mod segment;
-// #[allow(unused_variables)]
 mod taps_premiller;
 pub mod assigner;
 // mod assigners;
